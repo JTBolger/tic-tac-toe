a22b1b21a351e538a49a3b1b63c23c874387f905
JTBolger <112192375+JTBolger@users.noreply.github.com>
2023-09-20 18:11:36 -0500

    added squares
    added squares that can be clicked on to change the color, and alternated the color

---
+++ scripts/game.js
@@ -0,0 +1,157 @@
+var x = 1;
+var B1;
+var B2;
+var B3;
+var B4;
+var B5;
+var B6;
+var B7;
+var B8;
+var B9;
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var box6 = document.getElementById("box6");
+var box7 = document.getElementById("box7");
+var box8 = document.getElementById("box8");
+var box9 = document.getElementById("box9");
+
+function inputOne() {
+    if (x == 1) {
+        box1.style.backgroundColor = "#698db8b9";
+        box1.style.opacity = "1";
+        B1 = "blue";
+        x = 0;
+    }
+    else {
+        box1.style.backgroundColor = "#ee2b3fbe";
+        box1.style.opacity = "1";
+        B1 = "red";
+        x = 1;
+    }
+}
+function inputTwo() {
+    if (x == 1) {
+        box2.style.backgroundColor = "#698db8b9"
+        box2.style.opacity = "1"
+        B2 = "blue";
+        x= 0
+    }
+    else {
+        box2.style.backgroundColor = "#ee2b3fbe"
+        box2.style.opacity = "1"
+        B2 = "red"
+        x= 1
+    }
+}
+function inputThree() {
+    if (x == 1) {
+        box3.style.backgroundColor = "#698db8b9"
+        box3.style.opacity = "1"
+        B3 = "blue";
+        x= 0
+    }
+    else {
+        box3.style.backgroundColor = "#ee2b3fbe"
+        box3.style.opacity = "1"
+        B3 = "red"
+        x= 1
+    }
+}
+function inputFour() {
+    if (x == 1) {
+        box4.style.backgroundColor = "#698db8b9"
+        box4.style.opacity = "1"
+        B4 = "blue";
+        x = 0
+    }
+    else {
+        box4.style.backgroundColor = "#ee2b3fbe"
+        box4.style.opacity = "1"
+        B4 = "red"
+        x = 1
+    }
+}
+function inputFive() {
+    if (x == 1) {
+        box5.style.backgroundColor = "#698db8b9"
+        box5.style.opacity = "1"
+        B5 = "blue";
+        x= 0
+    }
+    else {
+        box5.style.backgroundColor = "#ee2b3fbe"
+        box5.style.opacity = "1"
+        B5 = "red"
+        x= 1
+    }
+}
+function inputSix() {
+    if (x == 1) {
+        box6.style.backgroundColor = "#698db8b9"
+        box6.style.opacity = "1"
+        B6 = "blue";
+        x= 0
+    }
+    else {
+        box6.style.backgroundColor = "#ee2b3fbe"
+        box6.style.opacity = "1"
+        B6 = "red"
+        x= 1
+    }
+}
+function inputSeven() {
+    if (x == 1) {
+        box7.style.backgroundColor = "#698db8b9"
+        box7.style.opacity = "1"
+        B7 = "blue";
+        x= 0
+    }
+    else {
+        box7.style.backgroundColor = "#ee2b3fbe"
+        box7.style.opacity = "1"
+        B7 = "red"
+        x= 1
+    }
+}
+function inputEight() {
+    if (x == 1) {
+        box8.style.backgroundColor = "#698db8b9"
+        box8.style.opacity = "1"
+        B8 = "blue";
+        x= 0
+    }
+    else {
+        box8.style.backgroundColor = "#ee2b3fbe"
+        box8.style.opacity = "1"
+        B8 = "red"
+        x= 1
+    }
+}
+function inputNine() {
+    if (x == 1) {
+        box9.style.backgroundColor = "#698db8b9"
+        box9.style.opacity = "1"
+        B9 = "blue";
+        x= 0
+    }
+    else {
+        box9.style.backgroundColor = "#ee2b3fbe"
+        box9.style.opacity = "1"
+        B9 = "red"
+        x= 1
+    }
+}
+
+function endGame() {
+
+}
+
+do {
+    if (B1 == "blue" && B2 == "blue" && B3 == "blue") {
+        endGame()
+    }
+} 
+while (i < 1)
