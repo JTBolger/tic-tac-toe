7b4c4dfdd6240941a274a16b8caa1166bfcd9357
JTBolger <112192375+JTBolger@users.noreply.github.com>
2023-09-21 13:50:44 -0500

    bug fix
    fixed bug where players could take over another tile that is already claimed

--- scripts/game.js
+++ scripts/game.js
@@ -20,147 +20,165 @@ var box8 = document.getElementById("box8");
 var box9 = document.getElementById("box9");
 
 function inputOne() {
-    if (x == 1) {
-        box1.style.backgroundColor = "#698db8b9";
-        box1.style.opacity = "1";
-        B1 = "blue";
-        x = 0;
-        testForWin()
-    }
-    else {
-        box1.style.backgroundColor = "#ee2b3fbe";
-        box1.style.opacity = "1";
-        B1 = "red";
-        x = 1;
-        testForWin()
+    if (B1 != "blue" && B1 != "red") {
+        if (x == 1) {
+            box1.style.backgroundColor = "#698db8b9";
+            box1.style.opacity = "1";
+            B1 = "blue";
+            x = 0;
+            testForWin()
+        }
+        else {
+            box1.style.backgroundColor = "#ee2b3fbe";
+            box1.style.opacity = "1";
+            B1 = "red";
+            x = 1;
+            testForWin()
+        }
     }
 }
 function inputTwo() {
-    if (x == 1) {
-        box2.style.backgroundColor = "#698db8b9"
-        box2.style.opacity = "1"
-        B2= "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box2.style.backgroundColor = "#ee2b3fbe"
-        box2.style.opacity = "1"
-        B2= "red"
-        x= 1
-        testForWin()
+    if (B2 != "blue" && B2 != "red") {
+        if (x == 1) {
+            box2.style.backgroundColor = "#698db8b9"
+            box2.style.opacity = "1"
+            B2= "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box2.style.backgroundColor = "#ee2b3fbe"
+            box2.style.opacity = "1"
+            B2= "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputThree() {
-    if (x == 1) {
-        box3.style.backgroundColor = "#698db8b9"
-        box3.style.opacity = "1"
-        B3 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box3.style.backgroundColor = "#ee2b3fbe"
-        box3.style.opacity = "1"
-        B3 = "red"
-        x= 1
-        testForWin()
+    if (B3 != "blue" && B3 != "red") {
+        if (x == 1) {
+            box3.style.backgroundColor = "#698db8b9"
+            box3.style.opacity = "1"
+            B3 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box3.style.backgroundColor = "#ee2b3fbe"
+            box3.style.opacity = "1"
+            B3 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputFour() {
-    if (x == 1) {
-        box4.style.backgroundColor = "#698db8b9"
-        box4.style.opacity = "1"
-        B4 = "blue";
-        x = 0
-        testForWin()
-    }
-    else {
-        box4.style.backgroundColor = "#ee2b3fbe"
-        box4.style.opacity = "1"
-        B4 = "red"
-        x = 1
-        testForWin()
+    if (B4 != "blue" && B4 != "red") {
+        if (x == 1) {
+            box4.style.backgroundColor = "#698db8b9"
+            box4.style.opacity = "1"
+            B4 = "blue";
+            x = 0
+            testForWin()
+        }
+        else {
+            box4.style.backgroundColor = "#ee2b3fbe"
+            box4.style.opacity = "1"
+            B4 = "red"
+            x = 1
+            testForWin()
+        }
     }
 }
 function inputFive() {
-    if (x == 1) {
-        box5.style.backgroundColor = "#698db8b9"
-        box5.style.opacity = "1"
-        B5 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box5.style.backgroundColor = "#ee2b3fbe"
-        box5.style.opacity = "1"
-        B5 = "red"
-        x= 1
-        testForWin()
+    if (B5 != "blue" && B5 != "red") {
+        if (x == 1) {
+            box5.style.backgroundColor = "#698db8b9"
+            box5.style.opacity = "1"
+            B5 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box5.style.backgroundColor = "#ee2b3fbe"
+            box5.style.opacity = "1"
+            B5 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputSix() {
-    if (x == 1) {
-        box6.style.backgroundColor = "#698db8b9"
-        box6.style.opacity = "1"
-        B6 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box6.style.backgroundColor = "#ee2b3fbe"
-        box6.style.opacity = "1"
-        B6 = "red"
-        x= 1
-        testForWin()
+    if (B6 != "blue" && B6 != "red") {
+        if (x == 1) {
+            box6.style.backgroundColor = "#698db8b9"
+            box6.style.opacity = "1"
+            B6 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box6.style.backgroundColor = "#ee2b3fbe"
+            box6.style.opacity = "1"
+            B6 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputSeven() {
-    if (x == 1) {
-        box7.style.backgroundColor = "#698db8b9"
-        box7.style.opacity = "1"
-        B7 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box7.style.backgroundColor = "#ee2b3fbe"
-        box7.style.opacity = "1"
-        B7 = "red"
-        x= 1
-        testForWin()
+    if (B7 != "blue" && B7 != "red") {
+        if (x == 1) {
+            box7.style.backgroundColor = "#698db8b9"
+            box7.style.opacity = "1"
+            B7 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box7.style.backgroundColor = "#ee2b3fbe"
+            box7.style.opacity = "1"
+            B7 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputEight() {
-    if (x == 1) {
-        box8.style.backgroundColor = "#698db8b9"
-        box8.style.opacity = "1"
-        B8 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box8.style.backgroundColor = "#ee2b3fbe"
-        box8.style.opacity = "1"
-        B8 = "red"
-        x= 1
-        testForWin()
+    if (B8 != "blue" && B8 != "red") {
+        if (x == 1) {
+            box8.style.backgroundColor = "#698db8b9"
+            box8.style.opacity = "1"
+            B8 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box8.style.backgroundColor = "#ee2b3fbe"
+            box8.style.opacity = "1"
+            B8 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 function inputNine() {
-    if (x == 1) {
-        box9.style.backgroundColor = "#698db8b9"
-        box9.style.opacity = "1"
-        B9 = "blue";
-        x= 0
-        testForWin()
-    }
-    else {
-        box9.style.backgroundColor = "#ee2b3fbe"
-        box9.style.opacity = "1"
-        B9 = "red"
-        x= 1
-        testForWin()
+    if (B9 != "blue" && B9 != "red") {
+        if (x == 1) {
+            box9.style.backgroundColor = "#698db8b9"
+            box9.style.opacity = "1"
+            B9 = "blue";
+            x= 0
+            testForWin()
+        }
+        else {
+            box9.style.backgroundColor = "#ee2b3fbe"
+            box9.style.opacity = "1"
+            B9 = "red"
+            x= 1
+            testForWin()
+        }
     }
 }
 
